3457db5373235c153f1c0864f8ce8831db9b2ba0
Saka <sakamoshood@ymail.com>
2021-03-08 01:08:29 +0000

    saka_FormStore

--- walkthroughs/week-3-libraries/datastore/examples/todo-list/src/main/java/com/google/sps/servlets/NewTaskServlet.java
+++ walkthroughs/week-3-libraries/datastore/examples/todo-list/src/main/java/com/google/sps/servlets/NewTaskServlet.java
@@ -14,16 +14,19 @@
 
 package com.google.sps.servlets;
 
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.FullEntity;
 import com.google.cloud.datastore.KeyFactory;
-import java.io.IOException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 
@@ -35,6 +38,8 @@ public class NewTaskServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Sanitize user input to remove HTML tags and JavaScript.
     String title = Jsoup.clean(request.getParameter("title"), Whitelist.none());
+    String content = Jsoup.clean(request.getParameter("content"), Whitelist.none());
+
     long timestamp = System.currentTimeMillis();
 
     Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
@@ -42,6 +47,7 @@ public class NewTaskServlet extends HttpServlet {
     FullEntity taskEntity =
         Entity.newBuilder(keyFactory.newKey())
             .set("title", title)
+            .set("contenttho", content)
             .set("timestamp", timestamp)
             .build();
     datastore.put(taskEntity);
